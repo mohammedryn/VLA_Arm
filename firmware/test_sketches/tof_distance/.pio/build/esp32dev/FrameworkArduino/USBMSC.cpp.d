35e45e2ac823d07e2cda17a62b0175b7dd2c647b
mohammedryn <mohammedrah1289@gmail.com>
2026-05-22 00:09:23 +0530

    test: add VL53L5CX distance sketch

---
+++ firmware/test_sketches/tof_distance/.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.o: \
+ /home/m0mspagetthi/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ /home/m0mspagetthi/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/m0mspagetthi/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
